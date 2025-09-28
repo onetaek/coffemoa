0a8bf8369667ca10a5ecf9a112c53b1017dabf98
won taek oh <dnjsxorchlrh@naver.com>
2025-09-29 00:17:43 +0900

    init: spring-boot 프로젝트 생성(jdk-25, spring boot 3.5.6)

---
+++ backend/cafeerp/src/main/java/com/cafeerp/CafeerpApplication.java
@@ -0,0 +1,13 @@
+package com.cafeerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeerpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CafeerpApplication.class, args);
+  }
+
+}
---
+++ backend/cafeerp/src/test/java/com/cafeerp/CafeerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeerpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

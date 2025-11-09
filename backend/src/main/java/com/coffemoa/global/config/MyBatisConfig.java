513a1337cd658d2f581cbd2e6ff193b5805a3373
won taek oh <dnjsxorchlrh@naver.com>
2025-11-09 22:24:34 +0900

    build: mybatis 의존성 추가

---
+++ backend/src/main/java/com/coffemoa/global/config/MyBatisConfig.java
@@ -0,0 +1,30 @@
+package com.coffemoa.global.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@Configuration
+@MapperScan(basePackages = "com.coffemoa") // ✅ MyBatis Mapper 위치
+public class MyBatisConfig {
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+    factoryBean.setDataSource(dataSource);
+    factoryBean.setMapperLocations(
+        new PathMatchingResourcePatternResolver().getResources("classpath:/mappers/**/*.xml"));
+    return factoryBean.getObject();
+  }
+
+  // ✅ JPA + MyBatis 트랜잭션 공유용
+  @Bean
+  public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+}

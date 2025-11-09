15473ff2126081f5863bed8113da078ab320665e
won taek oh <dnjsxorchlrh@naver.com>
2025-11-09 22:25:10 +0900

    feat: 기초정보 테이블 틀잡기

---
+++ backend/src/main/java/com/coffemoa/domain/standard/entity/Unit.java
@@ -0,0 +1,21 @@
+package com.coffemoa.domain.standard.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+    name = "단위"
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Unit {
+
+}
